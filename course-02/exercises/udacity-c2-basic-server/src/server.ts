65275e76437c121c0b1b7aac3a51b64bc5b1c7ba
Kehinde Olawuwo <olawuwokenny@gmail.com>
2022-08-06 18:19:28 +0100

    Impementing Exercise Tasks

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -1,86 +1,123 @@
-import express, { Router, Request, Response } from 'express';
+import express, {Router, Request, Response} from 'express';
 import bodyParser from 'body-parser';
 
-import { Car, cars as cars_list } from './cars';
+import {Car, cars as cars_list} from './cars';
 
 (async () => {
-  let cars:Car[]  = cars_list;
+    let cars: Car[] = cars_list;
 
-  //Create an express application
-  const app = express(); 
-  //default port to listen
-  const port = 8082; 
-  
-  //use middleware so post bodies 
-  //are accessable as req.body.{{variable}}
-  app.use(bodyParser.json()); 
+    //Create an express application
+    const app = express();
+    //default port to listen
+    const port = 8082;
 
-  // Root URI call
-  app.get( "/", ( req: Request, res: Response ) => {
-    res.status(200).send("Welcome to the Cloud!");
-  } );
+    //use middleware so post bodies
+    //are accessable as req.body.{{variable}}
+    app.use(bodyParser.json());
 
-  // Get a greeting to a specific person 
-  // to demonstrate routing parameters
-  // > try it {{host}}/persons/:the_name
-  app.get( "/persons/:name", 
-    ( req: Request, res: Response ) => {
-      let { name } = req.params;
+    // Root URI call
+    app.get("/", (req: Request, res: Response) => {
+        res.status(200).send("Welcome to the Cloud!");
+    });
 
-      if ( !name ) {
-        return res.status(400)
-                  .send(`name is required`);
-      }
+    // Get a greeting to a specific person
+    // to demonstrate routing parameters
+    // > try it {{host}}/persons/:the_name
+    app.get("/persons/:name",
+        (req: Request, res: Response) => {
+            let {name} = req.params;
 
-      return res.status(200)
+            if (!name) {
+                return res.status(400)
+                    .send(`name is required`);
+            }
+
+            return res.status(200)
                 .send(`Welcome to the Cloud, ${name}!`);
-  } );
+        });
 
-  // Get a greeting to a specific person to demonstrate req.query
-  // > try it {{host}}/persons?name=the_name
-  app.get( "/persons/", ( req: Request, res: Response ) => {
-    let { name } = req.query;
+    // Get a greeting to a specific person to demonstrate req.query
+    // > try it {{host}}/persons?name=the_name
+    app.get("/persons/", (req: Request, res: Response) => {
+        let {name} = req.query;
 
-    if ( !name ) {
-      return res.status(400)
+        if (!name) {
+            return res.status(400)
                 .send(`name is required`);
-    }
+        }
 
-    return res.status(200)
-              .send(`Welcome to the Cloud, ${name}!`);
-  } );
+        return res.status(200)
+            .send(`Welcome to the Cloud, ${name}!`);
+    });
 
-  // Post a greeting to a specific person
-  // to demonstrate req.body
-  // > try it by posting {"name": "the_name" } as 
-  // an application/json body to {{host}}/persons
-  app.post( "/persons", 
-    async ( req: Request, res: Response ) => {
+    // Post a greeting to a specific person
+    // to demonstrate req.body
+    // > try it by posting {"name": "the_name" } as
+    // an application/json body to {{host}}/persons
+    app.post("/persons",
+        async (req: Request, res: Response) => {
 
-      const { name } = req.body;
+            const {name} = req.body;
 
-      if ( !name ) {
-        return res.status(400)
-                  .send(`name is required`);
-      }
+            if (!name) {
+                return res.status(400)
+                    .send(`name is required`);
+            }
 
-      return res.status(200)
+            return res.status(200)
                 .send(`Welcome to the Cloud, ${name}!`);
-  } );
+        });
 
-  // @TODO Add an endpoint to GET a list of cars
-  // it should be filterable by make with a query paramater
+    // @TODO Add an endpoint to GET a list of cars
+    // it should be filterable by make with a query paramater
+    app.get("/cars", async (req: Request, res: Response) => {
+        let {make} = req.query;
 
-  // @TODO Add an endpoint to get a specific car
-  // it should require id
-  // it should fail gracefully if no matching car is found
+        if (make){
+            res.status(200).send(cars.filter((car) => {
+                return car.make === make;
+            }));
+        }
+        return res.status(200).send(cars);
+    });
 
-  /// @TODO Add an endpoint to post a new car to our list
-  // it should require id, type, model, and cost
+    // @TODO Add an endpoint to get a specific car
+    // it should require id
+    // it should fail gracefully if no matching car is found
+    app.get("/cars/:id", async (req: Request, res: Response) => {
+        let {id} = req.params;
 
-  // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+        if (!id) {
+            return res.status(400)
+                .send(`id is required`);
+        }
+
+        let car = cars.filter((item) => {
+            return item.id === Number(id);
+        });
+
+        return res.status(200).send(car);
+    });
+
+    /// @TODO Add an endpoint to post a new car to our list
+    // it should require id, type, model, and cost
+    app.post("/cars", async (req: Request, res: Response) => {
+        let {id, type, model, cost} = req.body;
+
+        if (!id || !type || !model || !cost) {
+            return res.status(400)
+                .send(`id, type, model, and cost is required`);
+        }
+
+        let new_car: Car = {cost: cost, make: "bmw", model: model, type: type, "id": id};
+        cars.push(new_car);
+
+        return res.status(200).send(cars);
+    });
+
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`server running http://localhost:${port}`);
+        console.log(`press CTRL+C to stop server`);
+    });
 })();
